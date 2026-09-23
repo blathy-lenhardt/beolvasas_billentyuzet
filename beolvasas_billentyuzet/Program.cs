64cd83037e1edba9d41e219e58ec3a47d453c5ff
lenhardt.nimrod <lenhardt.nimrod@blathy.info>
2026-09-23 10:09:04 +0200

    Add project files.

---
+++ beolvasas_billentyuzet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beolvasas_billentyuzet
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
