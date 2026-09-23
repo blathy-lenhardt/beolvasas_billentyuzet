348ba745d2797cf0792b743f74e58c049cb782b8
lenhardt.nimrod <lenhardt.nimrod@blathy.info>
2026-09-23 10:27:27 +0200

    Elmélet

--- beolvasas_billentyuzet/Program.cs
+++ beolvasas_billentyuzet/Program.cs
@@ -10,6 +10,41 @@ namespace beolvasas_billentyuzet
 	{
 		static void Main(string[] args)
 		{
+			// Beolvasás billentyűzetről
+
+			/* C#-ban csak szöveg típusú adatot lehet beolvasni.
+			 * 
+			 * a = Console.ReadLine();
+			 * 
+			 * Ha számot szeretnénk bekérni, akkor konvertálni kell a megfelelő számtípusra a bekért szöveget.
+			 * 
+			 * ADATOK KOVERTÁLÁSA
+			 * 
+			 * Statikus konvertálás: már fordítási időben az egyik típusú adat átkonvertálódik egy másik típusra.
+			 * 
+			 * Példa:
+			 * int x = 3;
+			 * Console.WriteLine(x/4);
+			 * Eredmény: 0
+			 * 
+			 * Console.WriteLine((float)x/4);
+			 * Eredmény: 0.75
+			 * 
+			 * Hátránya: nem tud mit kezdeni azzal, ha szövegből szeretnénk számotak nyerni, vagy fordítva.
+			 * 
+			 * Convert class: Különböző alap típusok közötti konvertálására.
+			 * 
+			 * Pl.:
+			 * 
+			 * int e = 0;
+			 * e = Convert.ToInt32(Console.ReadLine());
+			 * 
+			 * double v = 0;
+			 * v = Convert.ToDouble(Console.ReadLine());
+			 * 
+			 * char k = ' ';
+			 * k = Convert.ToChar(Console.ReadLine());
+			 */
 		}
 	}
 }
